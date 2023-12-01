59698221c96206141dc73e76f93a4011cc526414
Patrick Anderson <119633209+patrick-s-anderson@users.noreply.github.com>
2023-11-30 18:44:13 -0800

    test

--- day-01/src/bin/part1.rs
+++ day-01/src/bin/part1.rs
@@ -4,8 +4,12 @@ fn main() {
     dbg!(output);
 }
 
-fn part1(input: &str) -> String {
+fn part1(input: &str) -> u32 {
     input.to_string()
+    .split("\n\n")
+    .map(|e| e.lines().map(|c| c.parse::<u32>().unwrap()).sum::<u32>())
+    .max()
+    .unwrap()
 }
 
 #[cfg(test)]
@@ -15,8 +19,20 @@ mod tests {
     #[test]
     fn it_works() {
         let result = part1(
-            "5"
-        );
-        assert_eq!(result, "5".to_string());
+"1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000");
+        assert_eq!(result, 24000);
     }
 }
