d0185f5bb7871831052ae5fe1c1fba5ccc14c41d
Patrick Anderson <119633209+patrick-s-anderson@users.noreply.github.com>
2023-11-30 18:46:29 -0800

    formatting

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
 # aoc-2023
 
-
-Will be putting in my solutions for each day as individual Rust files.
+Patrick's Solutions
--- day-01/src/bin/part1.rs
+++ day-01/src/bin/part1.rs
@@ -1,7 +1,7 @@
 fn main() {
     let input = include_str!("./input1.txt");
     let output = part1(input);
-    dbg!(output);
+    println!( "{}", output);
 }
 
 fn part1(input: &str) -> u32 {
