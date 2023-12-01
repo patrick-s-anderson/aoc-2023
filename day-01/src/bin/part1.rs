509d522b6979b4c7c9e23621d5917d5e65e388e7
Patrick Anderson <119633209+patrick-s-anderson@users.noreply.github.com>
2023-11-30 22:22:04 -0800

    part1

--- day-01/src/bin/part1.rs
+++ day-01/src/bin/part1.rs
@@ -1,15 +1,65 @@
+//use std::str::Chars;
+
 fn main() {
     let input = include_str!("./input1.txt");
     let output = part1(input);
     println!( "{}", output);
 }
 
-fn part1(input: &str) -> u32 {
-    input.to_string()
-    .split("\n\n")
-    .map(|e| e.lines().map(|c| c.parse::<u32>().unwrap()).sum::<u32>())
-    .max()
-    .unwrap()
+fn part1(input: &str) -> i32 {
+    let mut v1 = vec![];
+    let mut v2 = vec![];
+
+    let mut result = 0;
+    for line in input.to_string().lines() {
+        for c in line.chars() { 
+            if c.is_numeric() {
+                v1.push(c);
+                //result += c.to_digit(10).unwrap();
+                break;
+            }
+        }
+  }
+
+  for line in input.to_string().lines() {
+    for c in line.chars().rev() { 
+        if c.is_numeric() {
+            v2.push(c);
+            //result += c.to_digit(10).unwrap();
+            break;
+        }
+    }
+}
+
+for it in v1.iter().zip(v2.iter_mut()) {
+    let (c1, c2) = it;
+    let mut s = String::from("");
+    s.push(*c1);
+    s.push(*c2);
+    println!("{}",s);
+    result += s.parse::<i32>().unwrap();
+}
+
+// for c in v1.iter_mut() {
+//     let mut s = String::from("");
+//     s.push(c);
+//   }
+  result
+
+
+    // .map(|line| {
+    //     line.split_()
+    //         .map(|col| col.parse::<i32>())
+    //         .collect::<Result<Vec<_>, _>>()
+    // })
+    // .collect::<Result<Vec<_>, _>>()
+    // .unwrap_or_else(|err| panic!("Cannot parse a column: {}", err))
+
+
+    // .split("\n")
+    // .map(|e| e.lines().map(|c| c.parse::<u32>().unwrap()).sum::<u32>())
+    // .max()
+    // .unwrap()
 }
 
 #[cfg(test)]
@@ -19,20 +69,10 @@ mod tests {
     #[test]
     fn it_works() {
         let result = part1(
-"1000
-2000
-3000
-
-4000
-
-5000
-6000
-
-7000
-8000
-9000
-
-10000");
-        assert_eq!(result, 24000);
+"1abc2
+pqr3stu8vwx
+a1b2c3d4e5f
+treb7uchet");
+        assert_eq!(result, 142);
     }
 }
--- day-01/src/bin/part2.rs
+++ day-01/src/bin/part2.rs
@@ -1,9 +1,69 @@
 fn main() {
     let input = include_str!("./input1.txt");
     let output = part1(input);
-    dbg!(output);
+    println!( "{}", output);
 }
 
-fn part1(input: &str) -> String {
-    input.to_string()
+fn part1(input: &str) -> i32 {
+    let mut v1 = vec![];
+    let mut v2 = vec![];
+
+    let numbers = vec!["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+
+    let mut result = 0;
+    for line in input.to_string().lines() {
+        let owned = line.to_owned();
+        let index = numbers.iter().position(|&r| r == owned);
+        match index {
+            Some(index) => println!("{}", index),
+            None => println!("none"),
+
+        }
+        for c in line.chars() { 
+            if c.is_numeric() {
+                v1.push(c);
+                break;
+            }
+        }
+  }
+
+  for line in input.to_string().lines() {
+    for c in line.chars().rev() { 
+        if c.is_numeric() {
+            v2.push(c);
+            break;
+        }
+    }
+}
+
+for it in v1.iter().zip(v2.iter_mut()) {
+    let (c1, c2) = it;
+    let mut s = String::from("");
+    s.push(*c1);
+    s.push(*c2);
+    println!("{}",s);
+    result += s.parse::<i32>().unwrap();
+}
+
+  result
+
+
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = part1(
+"two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen");
+        assert_eq!(result, 281);
+    }
 }
